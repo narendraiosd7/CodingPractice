4bbd5576a9a7d83bb8c30c386b05a9b78a696595
Narendra Vadde <narendra>
2025-05-22 07:02:11 +0530

    Getting an element/element index from an array

---
+++ CodingPractice.playground/Pages/GetAnElementFromAnArray.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let array = [10,13,17,19,23,29,31]
+let index = 13
+let value = 43
+
+// Using index getting value
+if array.count > 0 && index < array.count {
+  print(array[index])
+}
+
+// Using value getting index
+
+if array.count > 0 {
+  for i in 0..<array.count - 1 {
+    if array[i] == value {
+      print("index", i)
+    }
+  }
+}
+
+//: [Next](@next)
