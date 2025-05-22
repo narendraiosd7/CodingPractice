a1a441882e4219c716446e3f94808ecf0268ebff
Narendra Vadde <narendra>
2025-05-22 16:24:16 +0530

    sum of al the elements within the array

---
+++ CodingPractice.playground/Pages/SumOfAllElements.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+let array = [16, 24, 1, 17, 12, 2, 10, 4, 5, 20]
+
+print("Array: \(array)")
+
+var total = 0
+
+for value in array {
+  total += value
+}
+
+print("Total: \(total)")
