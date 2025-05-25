a1dfc79300069af2a23b79a01b3f005b91e3650e
Narendra Vadde <narendra>
2025-05-26 05:26:10 +0530

    Difference between 2 sorted arrays

---
+++ DifferenceOfSortedArrays.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+let firstArray = [2, 6, 10, 15, 25]
+let secondArray = [3, 6, 7, 15, 20]
+
+func differenceOfArrays(_ firstArray: [Int], _ secondArray: [Int]) -> [Int] {
+  var thirdArray = [Int]()
+  var i = 0, j = 0
+  
+  while i < firstArray.count && j < secondArray.count {
+    if firstArray[i] < secondArray[j] {
+      thirdArray.append(firstArray[i])
+      i += 1
+    } else if firstArray[i] > secondArray[j] {
+      j += 1
+    } else {
+      i += 1
+      j += 1
+    }
+  }
+  
+  for i in i..<firstArray.count {
+    thirdArray.append(firstArray[i])
+  }
+  
+  return thirdArray
+}
+
+print(differenceOfArrays(firstArray, secondArray))
